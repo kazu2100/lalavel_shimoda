a1bcf2dd2d68785cd1232331db784a170594566d
kazu2100 <shimokazu21@gmail.com>
2023-02-16 15:55:11 +0900

    d

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -31,6 +31,11 @@
             </div>
             <div>
               <a href="{{ route('posts.edit', $post) }}">編集</a>
+              <form action="{{ route('posts.destroy', $post) }}" method="post">
+                @csrf 
+                @method('delete')
+                <button type="submit">削除</button>
+              </form>
             </div>
           </div>
         </div>
