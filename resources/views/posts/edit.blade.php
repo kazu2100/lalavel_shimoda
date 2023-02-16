6daa2db535c5772a0c53f6f0d0e802bdf32460a3
kazu2100 <shimokazu21@gmail.com>
2023-02-16 18:58:33 +0900

    l

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,21 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>投稿編集</title>
+    @vite(['resources/js/app.js'])
   </head>
-  <body>
+  <body style="padding: 60px 0;">
     <header>
-      <nav>
-        <div>
-          <a href="{{ route('posts.index') }}">投稿アプリ</a>
+      <nav class="navbar nabar-light bg-light fixed-top" style="height: 60px;">
+        <div class="container">
+          <a href="{{ route('posts.index') }}" class="navbar-brand">投稿アプリ</a>
         </div>
       </nav>
     </header>
     <main>
       <article>
-        <div>
-          <h1>投稿編集</h1>
+        <div class="container">
+          <h1 class="fs-2 my-3">投稿編集</h1>
           @if ($errors->any())
-            <div>
+            <div class="alert alert-danger">
                 <ul>
                     @foreach ($errors->all() as $error)
                         <li>{{ $error }}</li>
@@ -26,26 +27,26 @@
                 </ul>
             </div>
           @endif  
-          <div>
-            <a href="{{ route('posts.index') }}">&lt; 戻る</a>
+          <div class="mb-2">
+            <a href="{{ route('posts.index') }}" class="text-decoration-none">&lt; 戻る</a>
           </div>
           <form action="{{ route('posts.update', $post) }}" method="post">
             @csrf 
             @method('patch')
-            <div>
+            <div class="form-group mb-3">
               <label for="title">タイトル</label>
-              <input type="text" name="title" value="{{ old('title', $post->title) }}">
+              <input type="text" class="form-control" name="title" value="{{ old('title', $post->title) }}">
             </div>
-            <div>
+            <div class="form-group mb-3">
               <label for="content">本文</label>
-              <textarea name="content">{{ old('content', $post->content) }}</textarea>
+              <textarea class="form-control" name="content">{{ old('content', $post->content) }}</textarea>
             </div>
-            <button type="submit">更新</button>
+            <button type="submit" class="btn btn-outline-primary">更新</button>
         </div>
       </article>
     </main>
-    <footer>
-      <p>&copy; 投稿アプリ All rights reserved.</p>
+    <footer class="d-flex justify-content-center aligin-items-center bg-light fixed-bottom" >
+      <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
     </footer>
   </body>
 </html>
