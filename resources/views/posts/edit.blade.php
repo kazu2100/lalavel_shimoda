82aad16fc504443116e6d43530e86f52babde4f0
kazu2100 <shimokazu21@gmail.com>
2023-02-20 19:01:41 +0900

    a

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta charset="UTF-8">
@@ -49,4 +49,34 @@
       <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
     </footer>
   </body>
-</html>
+</html> -->
+
+@extends('layouts.app')
+@section('title', '投稿編集')
+@section('content')
+  @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+  @endif  
+  <div class="mb-2">
+    <a href="{{ route('posts.index') }}" class="text-decoration-none">&lt; 戻る</a>
+  </div>
+  <form action="{{ route('posts.update', $post) }}" method="post">
+    @csrf 
+    @method('patch')
+    <div class="form-group mb-3">
+      <label for="title">タイトル</label>
+      <input type="text" class="form-control" name="title" value="{{ old('title', $post->title) }}">
+    </div>
+    <div class="form-group mb-3">
+      <label for="content">本文</label>
+      <textarea class="form-control" name="content">{{ old('content', $post->content) }}</textarea>
+    </div>
+    <button type="submit" class="btn btn-outline-primary">更新</button>
+  </form>
+@endsection
